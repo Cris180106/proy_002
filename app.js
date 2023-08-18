5403690e2ebb7c553f469f858ef7dac24b694e4b
Cris180106 <cpimiento345@gmail.com>
2023-08-18 18:26:34 +0200

    numeros de operaciones cambiados y mensaje añadido

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const functions = require('./functions.js')
 console.log(functions);
 process.stdout.write("Ejercicio\n\n");
 
-console.log(functions.add(8,5));
-console.log(functions.substract(9,22));
-console.log(functions.multiply(7,9));
-console.log(functions.divide(4,0.9));
+console.log("funciones matematicas");
+
+console.log(functions.add(6,5));
+console.log(functions.substract(4,22));
+console.log(functions.multiply(2,9));
+console.log(functions.divide(8,0.9));
